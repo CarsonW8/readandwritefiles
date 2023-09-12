6061e5e49bcd86f2c508ff3e3b0f0d2276848b2e
Carson Waite <Carson_Waite1@baylor.edu>
2023-09-12 18:47:00 -0500

    sales report

--- customer_sales_report.py
+++ customer_sales_report.py
@@ -9,15 +9,21 @@ outfile = open('salesreport.csv', 'w')
 csv_file = csv.reader(salesdata)
 next(csv_file)
 
-outfile.write("Customer ID, Total\n")
+outfile.write("Customer ID,Total\n")
 
 for rec in csv_file:
     if custid == rec[0]:
-        total += rec [3] + rec[4] + rec[5]
+        total += float(rec[3]) + float(rec[4]) + float(rec[5])
+        #add recs3,4,5
     else:
-        outfile.write(custid, total)
-        #write custid and total to the outfile
-        #250, 200 (if 200 is total)
+        outfile.write(custid)
+        outfile.write(',')
+        outfile.write(f"{total:.2f}\n")
         custid = rec[0] #assign custid to the next one (251)
-        total = rec[3] + rec[4] + rec[5]
-#write the custid and total to the outfile for the last record (261)
+        total = float(rec[3]) + float(rec[4]) + float(rec[5])
+
+outfile.write(custid)
+outfile.write(',')
+outfile.write(f"{total:.2f}\n")
+
+outfile.close()
